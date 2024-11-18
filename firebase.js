d6d6289c2b38ce7666cfde66756069dc082bd120
Chase McElroy <ichasem@gmail.com>
2024-11-17 22:12:38 -0800

    Added firebase

---
+++ firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC7Exb5zuc9ODySDH8GJjOLTNKyy4P0YPc",
+  authDomain: "caroling-companion-d2ad6.firebaseapp.com",
+  databaseURL: "https://caroling-companion-d2ad6.firebaseio.com",
+  projectId: "caroling-companion-d2ad6",
+  storageBucket: "caroling-companion-d2ad6.firebasestorage.app",
+  messagingSenderId: "1015969883158",
+  appId: "1:1015969883158:web:e9c8333df40bfdc2bb8137"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
